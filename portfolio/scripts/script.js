2f8bcd00f7fb09efa3fd1bb919ba8d7b29e8206a
kavitha <kavitha@LAPTOP-QNFFIGO1>
2023-06-07 19:36:32 +0530

    first commit

---
+++ portfolio/scripts/script.js
@@ -0,0 +1,22 @@
+const navbar = document.querySelector('.header .navbar');
+const menuButton = document.querySelector('.header .menu');
+
+menuButton.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+});
+
+document.onscroll = () => {
+    if (window.scrollY > 0) {
+      document.querySelector('.header').classList.add('active');
+    } else {
+      document.querySelector('.header').classList.remove('active');
+    }
+  };
+  
+  document.onload = () => {
+    if (window.scrollY > 0) {
+      document.querySelector('.header').classList.add('active');
+    } else {
+      document.querySelector('.header').classList.remove('active');
+    }
+};
